cabc4bf5033dc68ae85ad07659eaf021c38385ec
brianexprianto <brianexprianto@gmail.com>
2022-05-14 08:15:38 +0700

    add view music player

---
+++ layouts/default.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer color="black" permenant app>
+      <v-img
+        class="ml-4 mt-4 mr-12 mb-6"
+        src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgBc0FhmRUUtoWx_ThrnSnV9iMZpYiW9k7Kjl7phnB_0WBYUfdhAaXywPlmfyKNeOiuaMRyeEHVM9ilVlrYy4lLEEtQ32EhoVF2GBUGPAz4dPfNTd2zyQxqwWE_I-8EOsv_5pR9tLH1HRAXMvMPEA9iinfSBK4TktSX_NCLZRjldstsKAhgGbhLxd81/s320/PurifyLogo.png"
+      ></v-img>
+      <v-list nav>
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact class="ma-1">
+          <v-list-item-action class="mr-4">
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-col>
+        <span class="text-button text-uppercase grey--text ml-2">Playlists</span>
+        <v-list-item class="pl-2">
+          <v-list-item-action class="mr-4">
+            <v-icon>mdi-plus</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Create Playlist</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item class="pl-2">
+          <v-list-item-action class="mr-4">
+            <v-icon>mdi-heart</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Liked Songs</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider class="ml-2 mt-2"></v-divider>
+        <v-list-item class="pl-2">
+          <v-list-item-action class="mr-4">
+            <v-icon>mdi-download-circle-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Install App</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-col>
+      
+    </v-navigation-drawer>
+    <v-app-bar flat style="background:#121212" app>
+      <v-row>
+        <v-btn icon>
+          <v-icon large>mdi-arrow-left-bold-circle</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon dark large>mdi-arrow-right-bold-circle</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-chip close close-icon="mdi-menu-down" class="mt-2 grey darken-3">
+          <v-avatar left>
+            <v-icon >mdi-account-circle</v-icon>
+          </v-avatar>
+          <span class="mr-2 font-weight-bold">BrianExprianto</span>
+        </v-chip>
+      </v-row>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt /> 
+      </v-container>
+    </v-main>
+    <v-footer app fixed>
+      <MusicPlayer></MusicPlayer>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import MusicPlayer from "@/components/music_player"
+export default {
+  components:{
+    MusicPlayer
+  },
+  data() {
+    return {
+      items: [
+        {
+          icon: "mdi-home",
+          title: "Home",
+          to: "/"
+        },
+        {
+          icon: "mdi-magnify",
+          title: "Search",
+          to: "/search"
+        },
+        {
+          icon: "mdi-playlist-music",
+          title: "Your library",
+          to: "/library"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <v-row>
+      <span class="text-h5 font-weight-bold">Recently Played</span>
+      <v-spacer></v-spacer>
+      <span class="caption grey--text mr-2">SEE ALL</span>
+    </v-row>
+    <v-row class="mt-5">
+      <v-card rounded v-for="item in musicList()" :key="item.title" class="ma-2">
+        <v-img :src="item.img" height="145" width="145" class="mx-4 mt-4"></v-img>
+        <v-card-title>{{item.title}}</v-card-title>
+        <v-card-subtitle class="mb-6">{{item.author}}</v-card-subtitle>
+      </v-card>
+    </v-row>
+    <v-row class="mt-12">
+      <v-col class="pa-0">
+        <div class="text-h5 font-weight-bold mb-1">Pupular Albums</div>
+        
+      </v-col>
+      <v-spacer></v-spacer>
+      <span class="caption grey--text mr-2">SEE ALL</span>
+    </v-row>
+    <v-row class="mt-5">
+      <v-card rounded v-for="item in musicList()" :key="item.title" class="ma-2">
+        <v-img :src="item.img" height="145" width="145" class="mx-4 mt-4"></v-img>
+        <v-card-title>{{item.title}}</v-card-title>
+        <v-card-subtitle class="mb-6">{{item.author}}</v-card-subtitle>
+      </v-card>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    music: [
+      {
+        title: "Manusia",
+        author: "Tulus",
+        img:
+          "https://i.scdn.co/image/ab67616d0000b273b55d26c578e30129b0a7e86e"
+      },
+      {
+        title: "Justice",
+        author: "Justin Bieber",
+        img: "https://i.scdn.co/image/ab67616d0000b273e6f407c7f3a0ec98845e4431"
+      },
+      {
+        title: "Sisa Rasa",
+        author: "Mahalini",
+        img: "https://i.scdn.co/image/ab67616d0000b273cf312a99e7451a59ef915f83"
+      },
+      {
+        title: "Tak Sanggup Melupa",
+        author: "Ziva Magnolya",
+        img:
+          "https://i.scdn.co/image/ab67616d0000b273e06baa46ed1930b96669f4c5"
+      },
+      {
+        title: "SOUR",
+        author: "Olivia Rodrigo",
+        img:
+          "https://i.pinimg.com/236x/54/79/44/54794436b71840391593e0eedf63f782.jpg"
+      },
+      {
+        title: "Terlanjur Mencinta",
+        author: "Tiara Andini",
+        img:
+          "https://i.scdn.co/image/ab67616d00001e02937d621f0025409ee44285d1"
+      }
+    ]
+  }),
+  methods:{
+    musicList(){
+      const newArr = this.music.slice()
+        for (let i = newArr.length - 1; i > 0; i--) {
+            const rand = Math.floor(Math.random() * (i + 1));
+            [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
+        }
+        return newArr
+    }
+  }
+};
+</script>
